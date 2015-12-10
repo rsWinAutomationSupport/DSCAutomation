8c755f324c81f01266cb52908ea99a45bb232b90
Alexei Andreyev <alexei.andreyev@rackspace.co.uk>
2015-12-10 08:55:56 +0000

    Adding Process-DSCClientRegistrations function

---
+++ functions/Process-DSCClientRegistrations.ps1
@@ -0,0 +1,45 @@
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Process-DSCClientRegistrations
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    Param
+    (
+        # Full path to registered client data file
+        [Parameter(Mandatory=$true)]
+        [string]
+        $ClientDataPath,
+        
+        [string]
+        $MOFDestPath = "$env:ProgramFiles\WindowsPowerShell\DscService\Configuration",
+        
+        [string]
+        $ClientConfigPath,
+        
+        [string]
+        $ClientConfigHashPath,
+        
+        [string]
+        $QueueName = "dscregistration"
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+    }
+    End
+    {
+    }
+}
+
