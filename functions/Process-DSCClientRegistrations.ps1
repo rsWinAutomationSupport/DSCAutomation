163b05dc3006147f4e22f32c9ac199343303ddc9
nick-o <engelen.nico@googlemail.com>
2015-12-22 14:39:41 +0000

    first draft of Function to parse messages from queue

--- functions/Process-DSCClientRegistrations.ps1
+++ functions/Process-DSCClientRegistrations.ps1
@@ -29,17 +29,85 @@ function Process-DSCClientRegistrations
         $ClientConfigHashPath,
         
         [string]
-        $QueueName = "dscregistration"
+        $QueueName = "dscregistration",
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $CertificatesFolderPath
     )
 
-    Begin
+    [Reflection.Assembly]::LoadWithPartialName("System.Messaging") | Out-Null
+    $queue = New-Object System.Messaging.MessageQueue ".\private$\$QueueName"
+    $queue.Formatter.TargetTypeNames = ,"System.String"
+    do
     {
-    }
-    Process
-    {
-    }
-    End
-    {
-    }
+        $msg = $null
+        try
+        {
+            $msg = $queue.Receive((New-TimeSpan -Seconds 2))
+        }
+        catch [System.Messaging.MessageQueueException]
+        {
+            if ( $_.Exception.ToString().Contains("Timeout for the requested operation has expired.") )
+            {
+                Write-Verbose "No messages found after specified timeout"
+                return
+            }
+            else
+            {
+                throw $_
+            }
+        }
+
+        if ($msg)
+        {
+            Write-Verbose "$($msg.Count) message(s) received"
+            $bodyJson = $msg.Body
+            $body = $bodyJson | ConvertFrom-Json
+            $registrationCert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
+            $certData = [System.Convert]::FromBase64String($body.publicCert)
+            try
+            {
+                $registrationCert.Import($certData)
+                Write-Verbose "Cert Import successful, Thumbprint: $($cert.Thumbprint)"
+            }
+            catch [System.Security.Cryptography.CryptographicException]
+            {
+                Write-Verbose "Could not import Certificate from message"
+                #$_
+            }
+
+            if ( $registrationCert.Thumbprint )
+            {
+                $destinationCert = Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object { $_.Thumbprint -eq $registrationCert.Thumbprint }
+                if ( $destinationCert )
+                {
+                    Write-Verbose "Client Certificate already exists in destination store"
+                }
+                else
+                {
+                    Write-Verbose "Adding Client Certificate to destination store"
+                    $store = Get-Item -Path Cert:\LocalMachine\My
+                    $store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::MaxAllowed)
+                    $store.Add($registrationCert)
+                    $store.Close()
+                }
+                
+                $destinationFile = "$CertificatesFolderPath\$($body.uuid).cer"
+                if ( (Test-Path $destinationFile) )
+                {
+                    Write-Verbose "Destination Certificate file already exists"
+                }
+                else
+                {
+                    $CertificateFileData = $cert.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Cert)
+                    [System.IO.File]::WriteAllBytes($destinationCert, $CertificateFileData)
+                }
+            }
+        }
+
+
+     } while ($msg)
+
 }
 
